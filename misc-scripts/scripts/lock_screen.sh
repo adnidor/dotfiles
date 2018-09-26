88b756b8c02c43d9a883bd4a46b5da426dc465d6
Yannik Enss <me@adnidor.de>
2018-09-26 10:44:57 +0200

    use local i3lock-wrapper

--- misc-scripts/scripts/lock_screen.sh
+++ misc-scripts/scripts/lock_screen.sh
@@ -6,5 +6,5 @@ revert() {
 trap revert SIGHUP SIGINT SIGTERM
 (sleep 5; xset dpms force off) &
 curl -H "Content-Type: application/json" -d '{"entity_id": "light.schreibtischlampe"}' -X POST http://homeassistant.yannik.intern.yannikenss.de:8123/api/services/light/turn_off &
-i3lock-wrapper -l -n
+~/scripts/i3lock-wrapper -l -n
 revert
